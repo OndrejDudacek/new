df6710db0fb5e41bde03f54a32ae05e81c23de06
Dudáček Ondřej <dudacek.on.2022@skola.ssps.cz>
2022-11-09 08:37:53 +0100

    first commit

---
+++ hello world/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool running = true;
+
+            while (running)
+            {
+                string vstup = Console.ReadLine();
+                int cislo = 0;
+                if (vstup == "n")
+                {
+                    break;
+                }
+                else
+                {
+                    cislo = int.Parse(vstup);
+                }
+
+
+                for (int i = 0; i < cislo; i++)
+                {
+                    Console.WriteLine("Cyklus" + (i + 1));
+                }
+            }
+
+            
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
